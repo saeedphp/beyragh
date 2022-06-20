2a0efbbc24317269209a634dc2500d001aa6e7fc
Pkpedram <pkpedram80@gmail.com>
2022-06-20 12:21:58 +0430

    tailwind CSS configed

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+       
+
+      },
+    },
+    screens: {
+      res1: "1410px",
+      res2: "1230px",
+    },
+  },
+  plugins: [],
+};
