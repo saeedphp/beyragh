4ba4d21e29e13ba500cdf73a04bccb931c6d6351
Pkpedram <pkpedram80@gmail.com>
2022-06-21 15:08:42 +0430

    Data Manager Added

--- redux/constants.js
+++ redux/constants.js
@@ -1,1 +1,10 @@
 export const serverUrl = 'https://beyragh.faradns.ir/api/v1.0/';
+export const ApiConfig = {
+    apiKey: '',
+    authDomain: 'beyragh.faradns.ir',
+    baseUrl: 'https://beyragh.faradns.ir/api/v1.0/',
+    //baseUrl: 'http://localhost:3030/api/v1',
+    // loginURL: 'user/login',
+    // otploginURL: 'user/otp/login',
+    // logoutURL: 'user/logout',
+  };
